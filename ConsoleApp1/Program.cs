80e29d5771fa660a73c0e2efbe5857fd42e629ba
Nicolas Castaño <nicolasm.castano@gmail.com>
2019-03-21 00:02:39 -0300

    Async main method and crawl

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,17 +5,19 @@
 
 using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace WebCrawler
 {
+    //Summary: Main class that runs the app 
     class Program
     {
-        public static void  Main(string[] args)
+        public static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             Crawler webCrawler = new Crawler("https://es.wikipedia.org/wiki/");
-            webCrawler.Start();
+            await webCrawler.Start();
             Console.ReadKey();
         }
     }
@@ -23,6 +25,7 @@ namespace WebCrawler
     //Summary: Class Crawler models
     class Crawler
     {
+        object locking_webPagesVisited = new object();
         private int webPagesVisited = 0;
         private List<string> webpages;
         private Dictionary<string, int> wordCounter;
@@ -32,9 +35,25 @@ namespace WebCrawler
             wordCounter = new Dictionary<string, int>();
         }
 
-        public void Start()
+        public async Task Start()
         {
-            return;
+            await crawl(webpages[0]);
+        }
+
+        //Summary: crawl method into currentWebPage
+        public async Task crawl(string currentWebPage) {
+
+            //webPagesVisited needs to be blocked beacuse it is a shared resource
+            lock (locking_webPagesVisited)
+            {
+                webPagesVisited++;
+            }
+
+            //Request WebPage
+            HttpClient client = new HttpClient();
+            Console.WriteLine("Requesting the webpage {0}", currentWebPage);
+
+
         }
     }
 
